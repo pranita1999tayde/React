52619c92c6c43d1777eaada8fde8c32b7b696cac
pranita1999tayde <144302372+pranita1999tayde@users.noreply.github.com>
2023-11-01 20:18:42 +0530

    update

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Components/Home.jsx'
+
+function App() {
+  console.log("Hii form app file.")
+  return (
+    <div className="App">
+     <h1> Hiiii</h1>
+     <Home/>
+    </div>
+  )
+}
+
+export default App;
---
+++ frontend/src/Components/Home.jsx
@@ -0,0 +1,9 @@
+function Home(){
+    
+    return(
+        <div>Home Counter : 0</div>
+    )
+}
+
+
+export default Home;
